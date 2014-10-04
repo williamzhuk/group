18783c63387317a36d85c036aea1846d25caaf41
limabeans <toomanytypos@gmail.com>
2014-10-04 06:33:44 -0700

    status

--- code.js
+++ code.js
@@ -80,20 +80,3 @@ function testAPI() {
     });
 }
 
-
-console.log("before");
-function status() {
-    FB.api(
-        "/10202198852007677",
-        function (response) {
-            if (response && !response.error) {
-                /* handle the result */
-                console.log(response);
-            }
-        }
-    );
-};
-
-status();
-
-console.log("after");
